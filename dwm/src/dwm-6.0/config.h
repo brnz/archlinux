319fcb16874817f68a185d4cb9f31bafa7d50e04
brian <brian@z.com>
2012-10-16 04:22:04 -0700

    auto update

--- dwm/src/dwm-6.0/config.h
+++ dwm/src/dwm-6.0/config.h
@@ -68,11 +68,13 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
+static const char *nyccmd[]   = { "urxvt", "-fn", "xft:nyc:pixel=10"};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY|ShiftMask,             XK_n,      spawn,          {.v = nyccmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
